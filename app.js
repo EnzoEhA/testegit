77757b0c5d303d2cec450fa59408c990e2a42482
ENZO HENRIQUE AMBRUSTER <enzo.hambruster@senacsp.edu.br>
2026-02-10 22:02:38 -0300

    inclui as Alterações dos arquivos app.js e teste.html

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 console.log("Olá mundo")
 const variavel = document.getElementById("root")
-variavel.innerHTML = "Meu primeiro código em Javascript. Já sou progomador"
+variavel.innerHTML = "Meu primeiro código em Javascript. Já sou progomador juniorzinho"
